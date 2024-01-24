885fca6ed743833202e3590e3154c5aff4d5a29f
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-01-24 11:44:58 +0100

    created db.json and structure of Select.js/ProjectForm.js. Implemented communication between json server and ProjectForm

--- project-react03/src/Components/Project/ProjectForm.js
+++ project-react03/src/Components/Project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import React from 'react';
 import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
@@ -5,11 +7,28 @@ import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm(props) {
+
+  const [categories, setCategories] = useState([])
+
+useEffect(() => {
+fetch("http://localhost:5000/categories", {
+  method: "GET",
+  headers: {
+    'Content-type': 'application/json'
+  }
+})
+.then((resp) => resp.json())
+.then((data) => {
+  setCategories(data)
+})
+.catch((err) => console.log(err))
+}, [])
+
   return (
     <form className={styles.form}>
       <Input type="text" text="Name of the project" name="name" placeholder="Insert the name of the project" />
       <Input type="number" text="Budget of the project" name="Budget" placeholder="Insert the total budget" />
-      <Select name="category_id" text="Select a category" />
+      <Select name="category_id" text="Select a category" options={categories} />
       <SubmitButton text={props.btnText} />
     </form>
   );
