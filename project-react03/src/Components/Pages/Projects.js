d54d3e4a93f0db40d996145563457c05b59a07cf
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-01-25 22:51:52 +0100

    Testing db.json (success), and message from project successfuly added.

--- project-react03/src/Components/Pages/Projects.js
+++ project-react03/src/Components/Pages/Projects.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Message from '../layout/Message'
+import { useLocation } from 'react-router-dom';
 
 function Projects() {
+
+  const location = useLocation()
+  let message = ''
+  if(location.state) {
+    message = location.state.message
+  } 
+
   return (
     <div>
-      <h1>Projects</h1>
+      <h1>My Projects</h1>
+      {message && <Message type="success" msg ={Message} />}
     </div>
   )
 }
